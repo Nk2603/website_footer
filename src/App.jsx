60d0d7e493d14e2df344b8baa9a8a86e64e75e2e
Nk2603 <thakrannisha2005@gmail.com>
2024-06-07 13:15:00 +0530

    web-footer

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,70 @@ function App() {
   return (
     <>
     <div className='mainfooter'>
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+          <div className='firstfooter'>
+                <h2>Laxmi Devi Institute of </h2>
+                <h2>Engineering & Technology</h2>
+              <p>
+                <br />
+                "Alwar-Tijara-Delhi Highway"
+                <br/>
+                "Chikani, Alwar, Rajasthan"
+                <br />
+                "India - 301028"
+              </p>
+              <span ><h4>+91-7073477274</h4></span>
+              <span ><h4>+91-9829103101</h4></span>
+              <a href="">director@lietalwar.org</a>
+          </div>
+          <div className='secondfooter'>
+                <h2>Quick Links</h2>
+                <div style={{height:'1px',width:'300px',backgroundColor:'lightgreen'}}></div>
+                <br />
+                 <a href="">About Us</a>
+                 <br />
+                 <br />
+                 <a href="">Amissions</a>
+                 <br />
+                 <br />
+                 <a href="">Apply @ LIET</a>
+                 <br />
+                 <br />
+                 <a href="">Boar of management</a>
+                 <br />
+                 <br />
+                 <a href="">Grievance Reressal</a>
+          </div>
+          <div className='thirdfooter'>
+              <h2>Courses</h2>
+              <div style={{height:'1px',width:'300px',backgroundColor:'lightgreen'}}></div> 
+              <br />          
+                <a href="">B.Tech</a>
+                <br />
+                <br />
+                <a href="">M.Tech</a>
+                <br />
+                <br />
+                <a href="">MBA</a>
+                <br />
+                <br />
+                <a href="">Polytechnic</a>
+          </div>
+          <div className='fourthfooter'>
+              <h2>External Links</h2>
+              <div style={{height:'1px',width:'300px',backgroundColor:'lightgreen'}}></div>
+              <br />
+              <a href="">AICTE</a>
+              <br />
+              <br />
+              <a href="">BTU</a>
+              <br />
+              <br />
+              <a href="">BTER</a>
+              <br />
+              <br />
+              <a href="">DELNET</a>
+          </div>
+      </div>
     </>
   )
 }
